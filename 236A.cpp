e16178d04661411d1db809f871a46d6f515c40a7
venom827 <gvenom680@gmail.com>
2025-08-17 13:37:33 +0530

    Was kinda unsatisfied with the commits so decided to tackle another question 236A, for now all the examples are working, let's hope the tests work as well

---
+++ 236A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string userName;
+    int counter = 0;
+    cin >> userName;
+    for (int i = 0; i < userName.length(); i++){
+        counter++;
+        int j = i+1;
+        for ( j; j < userName.length(); j++ ){
+            if (userName[i] == userName[j]){
+                counter--;
+            }
+        }
+    }
+    if (counter%2 == 0){
+        cout << "CHAT WITH HER!";
+    }
+    else{
+        cout << "IGNORE HIM!";
+    }
+
+    return 0;
+}
