cff3f56412bdcaa0d8fcb792c6bc4fdb1654ce3b
venom827 <gvenom680@gmail.com>
2025-08-17 14:01:13 +0530

    Added coments explaining my thought process

--- 236A.cpp
+++ 236A.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <string>
 using namespace std;
+/*
+My solution to 236A. Pretty straightforward but the approach is to get the string,
+iterate through it, and increment a counter variable for every other character, 
+but decrement if there is another same character present in the string.
+This allows for counter to be canceled out for the first instance of each character 
+that is present multiple times in the string. And then get incremented on their other instance, 
+allowing us to get the value of distinct characters.
 
+As you might have guessed, this gives rise to an edgecase of when there are more than 2 usage of the same character 
+in the string or userName. For that, I did a very simple thing that took care of it. 
+You will find the comments below
+*/
 int main(){
 
     string userName;
@@ -13,11 +24,12 @@ int main(){
         for ( j; j < userName.length(); j++ ){
             if (userName[i] == userName[j]){
                 counter--;
-                break;
+                break;//When counter is decremented once, I don't allow the code
+                //to decrement it any more.
             }
         }
     }
-    if (counter%2 == 0){
+    if (counter%2 == 0){//very standard even-odd program
         cout << "CHAT WITH HER!";
     }
     else{
