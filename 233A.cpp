e4da206789d7eeb96ffe04a769f56b8a6c6625e6
venom827 <gvenom680@gmail.com>
2025-08-18 13:07:39 +0530

    LETSSSS GOOOOO the answer was right. First try babyyyy. For now just added some spaces but now I am gonna add some comments.

--- 233A.cpp
+++ 233A.cpp
@@ -5,20 +5,31 @@ int main() {
 
     int n;
     cin >> n;
+
     if (n%2 == 0){
+
         for (int i = 1; i <= n; i++){
+
+
             int counter = 0;
+
             if (i%2 == 0){
                 counter = i-1;
             }
+
             else{
                 counter = i+1;
             }
+
+
             cout << counter << " ";
         }
+
     }
     else{
+
         cout << -1;
+        
     }
 
 
