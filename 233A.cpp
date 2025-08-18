fa68623c5e30d3f92b8cd04d86a86a6c6159505d
venom827 <gvenom680@gmail.com>
2025-08-18 13:12:33 +0530

    Finished 233A with this commit message. Just added few comments to explain my reasoning.

--- 233A.cpp
+++ 233A.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
+/*
+This is my solution to 233A. The approach is pretty straighforward.
+I tried to understand what the perfect permutation was and it seems
+it was just counting all the numbers but swapping the places for all the
+consecutive 2 numbers along side each other. For ex - 1 2 becomes 2 1 and so on.
+I recreated this logic i program and it seemed to work out fine.
+*/
 
 int main() {
 
@@ -11,14 +18,14 @@ int main() {
         for (int i = 1; i <= n; i++){
 
 
-            int counter = 0;
+            int counter = 0;//the variable I will output each iteration
 
             if (i%2 == 0){
-                counter = i-1;
+                counter = i-1;//for ex 2 becomes 1
             }
 
             else{
-                counter = i+1;
+                counter = i+1;//for ex 1 becomes 2
             }
 
 
@@ -29,7 +36,7 @@ int main() {
     else{
 
         cout << -1;
-        
+
     }
 
 
