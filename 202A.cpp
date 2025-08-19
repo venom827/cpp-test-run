fb5624b0b0e4bdf212b0c3887919ae06385bc568
venom827 <gvenom680@gmail.com>
2025-08-19 08:18:55 +0530

    God this problem's a pain! Been trying it from tommorow but well had some stuff to do. But today i have time. I will solve this at any cost.

--- 202A.cpp
+++ 202A.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int main(){
+    int n1,n2;
+    n1 = 0;
+    n2 = 0;
     int icounter = 0;
     string counter;
     string s;
@@ -10,9 +13,30 @@ int main(){
     string s2 = "abcdefghijklmnopqrstuvwxyz";
     for (int i = 0; i < s.length(); i++){
         for (int j = i+1; j < s.length(); j++){
-            if (s[j] > s[i]){
-                counter = s[j];
+           for (int k = 0; k < s2.length(); k++){
+            if (s[i] == s2[k]){
+                if (n1>k){
+                    break;
+                }
+                else{
+                n1=k;
+                }
             }
+            else if(s[j] == s2[k]){
+                if (n2>k){
+                    break;
+                }
+                else{
+                n2=k;
+                }
+            }
+           }
+           if (n2 > n1){
+            counter =s2[n2];
+           }
+           else if( n1 > n2 ){
+            counter = s2[n1];
+           }
         }
     }
 
