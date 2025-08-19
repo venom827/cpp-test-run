fe96b45f2411e404bb7bd691dcc23452e8ab6da7
venom827 <gvenom680@gmail.com>
2025-08-19 18:22:32 +0530

    added comments for readability

--- 202A.cpp
+++ 202A.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+/*
+My solution to 202A. Mostly I only used a standard text book program for finding the max num
+Just here i find the max char. It's pretty easy if you're comparing some inputs, just assign any 
+one input to a counter variable. Then check if the other are bigger and if they are just assign them as counter
+else continue. Since I had to do this with char of a string I decided to assign smallest posssible value as 
+counter than compare against that for all chars in string. I was sidetracked a bit as can be seen by my commit history 
+so I only asked chatgpt for problems in my code that's when it suggested a concept I should've though of earlier. But only that
+part. Even the code I implemented on my own.
+*/
 int main(){
     
     int icounter = 0;
