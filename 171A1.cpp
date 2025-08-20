c18262efadc030f1be6a02041f5800e97561bbd0
venom827 <gvenom680@gmail.com>
2025-08-20 18:39:32 +0530

    finished 171A1. I think I did a similar problem in python so this was pretty easy. Plus I already know about matrix from highschool so it was pretty easy overall

---
+++ 171A1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin >> n;
+    int counter = n-1;
+    int matrix[100][100];
+    int primary_diagnol_sum = 0;
+    int secondary_diagnol_sum = 0;
+    int middle_row = (n-1)/2;
+    int middle_column = (n-1)/2;
+    int middle_column_sum = 0;
+    int middle_row_sum = 0;
+    int good_element_sum;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            int input;
+            cin >> input;
+            matrix[i][j] = input;
+        }
+    }
+
+    for (int i = 0; i < n; i++){
+        primary_diagnol_sum += matrix[i][i];
+    }
+    primary_diagnol_sum -= matrix[middle_row][middle_column];
+    for (int i = 0; i < n; i++){
+        secondary_diagnol_sum += matrix[i][counter];
+        counter--;
+    }
+    secondary_diagnol_sum -= matrix[middle_row][middle_column];
+    for (int i = 0; i < n; i++){
+        middle_row_sum += matrix[middle_row][i];
+    }
+    middle_row_sum -= matrix[middle_row][middle_column];
+    for (int i = 0; i < n; i++){
+        middle_column_sum += matrix[i][middle_column];
+    }
+    middle_column_sum -= matrix[middle_row][middle_column];
+    good_element_sum = primary_diagnol_sum + secondary_diagnol_sum + middle_row_sum + middle_column_sum + matrix[middle_row][middle_column];
+    cout << good_element_sum;
+    return 0;
+}
