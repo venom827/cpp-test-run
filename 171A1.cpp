1160cc829d94bb1d63d64d09096d598290bbac92
venom827 <gvenom680@gmail.com>
2025-08-20 18:42:33 +0530

    a

--- 171A1.cpp
+++ 171A1.cpp
@@ -14,6 +14,7 @@ int main(){
     int middle_column_sum = 0;
     int middle_row_sum = 0;
     int good_element_sum;
+
     //matrix constructor
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
@@ -22,11 +23,14 @@ int main(){
             matrix[i][j] = input;
         }
     }
+
+
 //Below are loops for each of the4 good matrix elements
 //Notice I removed middle element from each and added it seperately bcz that keeps it consistent with the theme of q plus it's the common element
     for (int i = 0; i < n; i++){
         primary_diagnol_sum += matrix[i][i];
     }
+
     primary_diagnol_sum -= matrix[middle_row][middle_column];
     for (int i = 0; i < n; i++){
         secondary_diagnol_sum += matrix[i][counter];
@@ -41,7 +45,12 @@ int main(){
         middle_column_sum += matrix[i][middle_column];
     }
     middle_column_sum -= matrix[middle_row][middle_column];
+
+
     good_element_sum = primary_diagnol_sum + secondary_diagnol_sum + middle_row_sum + middle_column_sum + matrix[middle_row][middle_column];
+    
     cout << good_element_sum;
+    
     return 0;
+
 }
