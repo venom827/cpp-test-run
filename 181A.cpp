9cb4fc9377bb9bd279b8cd25532203c01df7bc9c
venom827 <gvenom680@gmail.com>
2025-08-20 13:50:16 +0530

    Kinda did the q. Been working on it for a while but hopefully it's correct

--- 181A.cpp
+++ 181A.cpp
@@ -11,8 +11,7 @@ int main(){
     int c1 = 0;
     int pnts[4][2];
     char arr[100][100];
-    int row_distance;
-    int column_distance;
+    int column_length;
     for (int i = 0; i < n; i++){
         string input;
         cin >> input;
@@ -35,55 +34,62 @@ int main(){
             }
         }
     }
-    
-    
-
 
     for (int i = 0; i < n; i++){
-        for(int j = i+1; j < n; j++){
-           
-            if(pnts[i][0] == pnts[j][0]){
-                column_distance = abs(pnts[i][1] - pnts[j][1]);
-            }
-            if (pnts[i][1] == pnts[j][1]){
-                row_distance = abs(pnts[i][0] - pnts[j][0]);
+        for (int j = i+1; j <= m; j++){
+            if (pnts[i][0] == pnts[j][0]){
+                column_length = abs (pnts[i][1] - pnts[j][1]);
             }
         }
     }
-    int counter;
+
     for (int i = 0; i < 3; i++){
-        counter++;
-        for(int j = 0; j < 3; j++){
-            if(pnts[i][0] == pnts[j][0] && pnts[i][0] > pnts[2][0]){
-                pnt[0] = abs(pnts[i][0] + row_distance);
-            }
-            else if(pnts[i][0] == pnts[j][0] && pnts[i][0] < pnts[2][0]){
-                pnt[0] = abs(pnts[i][0] - row_distance);
-            }
-            else if (pnts[i][0] == pnts[j][0] && pnts[i][0] > pnts[1][0]){
-                pnt[0] = abs(pnts[i][0] + row_distance);
-
-            }
-            else if (pnts[i][0] == pnts[j][0] && pnts[i][0] < pnts[1][0]){
-                pnt[0] = abs(pnts[i][0] - row_distance);
-
-            }
-            else if(pnts[i][1] == pnts[j][1] && pnts[i][1] < pnts[2][1]){
-                pnt[1] = abs(pnts[i][1] + column_distance);
-            }
-            else if(pnts[i][1] == pnts[j][1] && pnts[i][1] > pnts[2][1]){
-                pnt[1] = abs(pnts[i][1] - column_distance);
-            }
-            else if (pnts[i][1] == pnts[j][1] && pnts[i][1] < pnts[1][0]){
-                pnt[1] = abs(pnts[i][1] - column_distance);
-
-            }
-            else if (pnts[i][1] == pnts[j][1] && pnts[i][1] > pnts[1][0]){
-                pnt[1] = abs(pnts[i][1] + column_distance);
-
-            }
+        int j;
+        for( j = 1; j < 3; j++){
+           if (pnts[i][0] == pnts[j][0] && pnts[i][0] > pnts[i-1][0]){
+                pnt[0] = pnts[i-1][0];
+                break;
+           }
+           else if (pnts[i][0] == pnts[j][0] && pnts[i][0] < pnts[i+2][0]){
+                pnt[0] = pnts[i+2][0];
+                break;
+           }
+        }
+        if (pnts[i][0] == pnts[j][0] && pnts[i][0] > pnts[i-1][0]){
+                break;
+        }
+        else if (pnts[i][0] == pnts[j][0] && pnts[i][0] < pnts[i+2][0]){
+                break;
         }
     }
+    for (int i = 0; i < 3; i++){
+        
+        int j;
+        for(j = 1; j < 3; j++){
+           if (pnts[i][0] == pnts[j][0] && pnts[i][0] > pnts[i-1][0]){
+                if (pnts[i-1][1] > 1 ){
+                    pnt[1] = pnts[i-1][1]-column_length;
+                }
+                else if(pnts[i-1][1] <= 1){
+                    pnt[1] = pnts[i-1][1]+column_length;
+                }
+                break;
+           }
+           else if (pnts[i][0] == pnts[j][0] && pnts[i][0] < pnts[i+2][0]){
+                if (pnts[i+2][1] > 1){
+                    pnt[1] = pnts[i+2][1] - column_length;
+                }
+                else if (pnts[i+2][1] <= 1){
+                    pnt[1] = pnts[i+2][1] + column_length;
+                }
+           }
+        
+        }
+        if(pnt[1] > 0 && pnt[1] < 100){
+            break;
+        }
+    }
+    
 
 
 
@@ -91,4 +97,4 @@ int main(){
 
 
     return 0;
-    }
+}
