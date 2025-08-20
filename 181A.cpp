d88818d6d081d07f14be9cbed9952d41b9e0b4da
venom827 <gvenom680@gmail.com>
2025-08-20 17:52:22 +0530

    Turned out i didnt need editorial but some time. Figured it out on the way home and did it first tryyyy

--- 181A.cpp
+++ 181A.cpp
@@ -1,106 +1,70 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace std;
+
 int main(){
-
-    int n,m;
-    string holder;
-    cin >> n >> m;
-    int pnt[2];
-    int c1 = 0;
-    int pnts[4][2];
     char arr[100][100];
-    int column_length;
-
+    int pnts[3][2];
+    int n,m;
+    int c1 = 0;
+    int counter = 0;
+    int r_coordinate;
+    int c_coordinate;
+    cin >> n >> m;
 
     for (int i = 0; i < n; i++){
         string input;
         cin >> input;
-
         for (int j = 0; j < m; j++){
-            arr[i][j] = input[j];
-            
-        }
-    
-    }
-            
 
+            arr[i][j] = input[j];
+        }
+    }
     for (int i = 0; i < n; i++){
         for (int j = 0; j < m; j++){
-            
-            if(arr[i][j] == '*'){
+            if (arr[i][j] == '*'){
                 pnts[c1][0] = i+1;
                 pnts[c1][1] = j+1;
                 c1++;
             }
         }
     }
-
-
     for (int i = 0; i < 2; i++){
         for (int j = i+1; j < 3; j++){
             if (pnts[i][0] == pnts[j][0]){
-                column_length = abs (pnts[i][1] - pnts[j][1]);
+                for (int k = 0; k < 3; k++){
+                    if (pnts[k][0] != pnts[i][0]){
+                        r_coordinate = pnts[k][0];
+                    }
+                }
             }
         }
     }
 
     for (int i = 0; i < 2; i++){
-        int j;
-        for( j = i+1; j < 3; j++){
-           if (pnts[i][0] == pnts[j][0] && pnts[i][0] > pnts[0][0]){
-                pnt[0] = pnts[0][0];
-                break;
-           }
-           else if (pnts[i][0] == pnts[j][0] && pnts[i][0] < pnts[2][0]){
-                pnt[0] = pnts[2][0];
-                break;
-           }
+        int k = 0;
+        for (int j = i+1; j < 3; j++){
+            if (pnts[i][1] == pnts[j][1]){
+                for (k; k < 3; k++){
+                    if (pnts[k][1] != pnts[i][1]){
+                        c_coordinate = pnts[k][1];
+                        break;
+                    }
+                }
+            }
+            if (pnts[k][1] != pnts[i][1]){
+                        break;
+                    }
         }
-        if (pnts[i][0] == pnts[j][0] && pnts[i][0] > pnts[0][0]){
-                break;
-        }
-        else if (pnts[i][0] == pnts[j][0] && pnts[i][0] < pnts[2][0]){
-                break;
-        }
+        if (pnts[k][1] != pnts[i][1]){
+                        break;
+                    }
     }
 
+        cout << r_coordinate << " " << c_coordinate;
 
-    for (int i = 0; i < 2; i++){
-        
-        int j;
-        for(j = i+1; j < 3; j++){
-           if (pnts[i][0] == pnts[j][0] && pnts[i][0] > pnts[0][0]){
-                if (pnts[0][1] > 1 ){
-                    pnt[1] = pnts[0][1]-column_length;
-                }
-                else if(pnts[0][1] <= 1){
-                    pnt[1] = pnts[0][1]+column_length;
-                }
-                break;
-           }
-           else if (pnts[i][0] == pnts[j][0] && pnts[i][0] < pnts[2][0]){
-                if (pnts[2][1] > 1){
-                    pnt[1] = pnts[2][1] - column_length;
-                }
-                else if (pnts[2][1] <= 1){
-                    pnt[1] = pnts[2][1] + column_length;
-                }
-                break;
-           }
-        
-        }
-        if(pnts[i][0] == pnts[j][0] && pnts[i][0] > pnts[0][0]){
-            break;
-        }
-        else if(pnts[i][0] == pnts[j][0] && pnts[i][0] < pnts[2][0]){
-            break;
-        }
-    }
+
     
-
-    cout << pnt[0]<< " " << pnt[1];
 
     return 0;
 }
