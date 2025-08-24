f18d24d90e2762add575ccb34fa494199062a723
venom827 <gvenom680@gmail.com>
2025-08-24 17:01:02 +0530

    had an old file so solved it

---
+++ 71A.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+    int n;
+    int counter1 = 0;
+    int counter2 = 0;
+    vector <string> words;
+    cin >> n;
+    for (int i=0;i<n;i++){
+        string input;
+        cin >> input;
+        if (input.length() > 10){
+            counter1 = input.length() - 2;
+            words.push_back(input[0] + to_string(counter1) + input[input.length()-1]);
+        }
+        else{
+            words.push_back(input);
+        }
+        
+    }
+    for (string i:words){
+        cout << i << endl;
+    }
+}
