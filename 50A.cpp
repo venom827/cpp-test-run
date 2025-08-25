b1e0542b013bba4e529eba3328daf6388b10362e
venom827 <gvenom680@gmail.com>
2025-08-25 13:46:48 +0530

    Did 50A for the commit streak

---
+++ 50A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int m,n;
+    cin >> n>>m;
+    int size = m*n;
+    int number_of_sq = size/2;
+    cout << number_of_sq;
+
+
+    return 0;
+}
